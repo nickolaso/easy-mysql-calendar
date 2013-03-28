f98a55f2982359a6a09b8bb041c8aafa7582c048
Nickolas Ormond <nick@nertworks.com>
2013-03-28 16:55:25 -0400

    Revert "Easy Mysql Calendar"
    This reverts commit 4929abd40fbf8469efb7921b6e4ad7d478e312ce.

--- htdocs/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con = mysql_connect("localhost","Database_Username","Database_password");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
-mysql_select_db("Database_name", $con);
-?>
